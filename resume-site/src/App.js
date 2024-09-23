2f27a62a927a5bf80fb21f925b9c08dbfd220152
Benjamin Bell <dtytrewq@hotmail.com>
2024-09-23 14:47:55 -0400

    changed github optimization

--- resume-site/src/App.js
+++ resume-site/src/App.js
@@ -1,10 +1,9 @@
 // src/App.js
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Header from './components/Header';
-import Footer from './components/Footer'; // Import the Footer component
 
 function App() {
   return (
@@ -15,7 +14,6 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
-      <Footer /> {/* Include the Footer component here */}
     </Router>
   );
 }
