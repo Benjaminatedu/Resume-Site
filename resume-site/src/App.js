2db0df6f4f635444443233c00d988bd096469a9a
Benjamin Bell <dtytrewq@hotmail.com>
2024-09-23 16:04:48 -0400

    Added a sticky footer

--- resume-site/src/App.js
+++ resume-site/src/App.js
@@ -1,21 +1,25 @@
+// src/App.js
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { Box, Flex } from '@chakra-ui/react';
 
 function App() {
   return (
-    <>
+    <Flex direction="column" minHeight="100vh"> {/* Flex container with column direction */}
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-      </Routes>
-      <Footer />
-    </>
+      <Box flex="1"> {/* Main content area that grows */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </Box>
+      <Footer /> {/* Footer will stay at the bottom */}
+    </Flex>
   );
 }
 
